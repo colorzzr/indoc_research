0f4e692b4965868522afee8fdd328d80dcb4f36d
colorzzr <colorzzr@gmail.com>
2019-03-25 10:55:55 -0400

    finish the error shooting and advanced function

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 
-
 # Analysis Data
-Parse CSV data
+
+This is the program take the csv file that contain the sepal length, sepal width, petal length, petal width, class. It outputs the average sepal length and petal length with a plotted graph
 
 
 # Geting Start
 
-The program use the Python3 to process all the data, to install Python3:
+The program uses the Python3 to process all the data, to install Python3:
 ```
 	$ sudo apt-get update
 	$ sudo apt-get install python3.6
@@ -36,21 +36,26 @@ Below are the detail of each function
 
 ---
 
-## def do_polt(file_name: str):
+## do_polt(file_name: str):
 
 The function take the user input calls all functions below to process the data in csv file and output the png for the user
 
 ---
 
-## def read_csv_file(file_name: str) -> List[List[str]]:
+## read_csv_file(file_name: str) -> List[List[str]]:
 
 function would take the arg in the terminal and open it for the function `parse_csv_data`
 
 ---
 
-## def parse_csv_data(data:List[List[str]]) -> dict:
+## parse_csv_data(data:List[List[str]]) -> dict:
 
 function read line by line throughout the file and put the required data into the dictionary. the key of dictionary is the class of the flower. The format of scv must follow below:
+
+
+	| sepal_length | sepal_width | petal_length | petal_width | class |
+	|--------------|-------------|--------------|-------------|-------|
+	| 	  float    |    float    | 	   float    | 	  float   |  str  |
 
 	- Attribute 1: sepal length in cm
 	- Attribute 2: sepal width in cm
@@ -63,16 +68,27 @@ function read line by line throughout the file and put the required data into th
 
 ### Error Shooting:
 
+If the csv is not contain the correct type above, the program would raise the error `csv file has wrong format, please check the readme!`
+
 ---
 
+## parse_csv_data_adv(data:List[List[str]]) -> dict:
 
-## def proccess_data(data:dict) -> (List[float], List[float], List[str]):
+This function is advanced for not only limit the three class above. If you want to calculate other classes data. please use this function
+
+### Error Shooting:
+
+If the csv is not contain the correct type above, the program would raise the error `csv file has wrong format, please check the readme!`
+
+---
+
+## proccess_data(data:dict) -> (List[float], List[float], List[str]):
 
 Function takes the analysised data.It calculate and return the array containning the average of sepal length, petal length and corresponding to each class
 
 ---
 
-## def plot_graph(sepal_arr:List[float], petal_arr:List[float], class_arr:List[str]):
+## plot_graph(sepal_arr:List[float], petal_arr:List[float], class_arr:List[str]):
 
 Function using `python-matplotlib` to plot the graph and output png file called `data.png`
 
--- main.py
+++ main.py
@@ -24,26 +24,48 @@ def parse_csv_data(data:List[List[str]]) -> dict:
                    "Iris-versicolor": [],
                    "Iris-virginica": []}
 
-    # <-------------------------------TO DO ERROR CHECKING
+
     for x in data:
-        # print(x)
-        # print(float(x[0]), float(x[1]), float(x[2]), float(x[3]), x[4])
-        temp = [float(x[0]), float(x[1]), float(x[2]), float(x[3])]
+        try:
+            temp = [float(x[0]), float(x[1]), float(x[2]), float(x[3])]
+        except:
+            print("csv file has wrong format, please check the readme!")
+            exit(1)
+
         if x[4] == "Iris-setosa":
             format_data["Iris-setosa"].append(temp)
         elif x[4] == "Iris-versicolor":
             format_data["Iris-versicolor"].append(temp)
-        else:
+        elif x[4] == "Iris-virginica":
             format_data["Iris-virginica"].append(temp)
 
-    # for cla in format_data:
-    #     print(cla)
-    #     for x in format_data[cla]:
-    #         print(x)
-    #
-    #     print("\n")
-
     return format_data
+
+
+# advance function it would not limit to the only class provided in data.csv
+def parse_csv_data_adv(data:List[List[str]]) -> dict:
+    format_data = {}
+
+    for x in data:
+        try:
+            temp = [float(x[0]), float(x[1]), float(x[2]), float(x[3])]
+        except:
+            print("csv file has wrong format, please check the readme!")
+            exit(1)
+
+        print(temp, format_data.get(x[4]))
+        # if the key class is not there create the arr
+        if format_data.get(x[4]) == None:
+            format_data[x[4]] = []
+        #     format_data[x[4]].append(temp)
+        # else:
+
+        format_data[x[4]].append(temp)
+
+        print(format_data)
+    return format_data
+
+
 
 # loop over what we fetch in csv and return the average of sepal length
 # petal length and corrospond to class
@@ -65,18 +87,27 @@ def proccess_data(data:dict) -> (List[float], List[float], List[str]):
 
         # get avg
         count = len(data[cla])
-        sepal_avg = round(sepal_length / count, 3)
-        petal_avg = round(petal_length / count, 3)
+        sepal_avg = 0
+        petal_avg = 0
+
+        # if we dont have target class
+        if count != 0:
+            sepal_avg = round(sepal_length / count, 3)
+            petal_avg = round(petal_length / count, 3)
+
+        # output the result
         print("Class: ", cla)
         print("Average sepal length: ", sepal_avg)
         print("Average petal length: ", petal_avg)
         print("\n")
 
+        # push into arr
         sepal_arr.append(sepal_avg)
         petal_arr.append(petal_avg)
         class_arr.append(cla)
 
     return sepal_arr, petal_arr, class_arr
+
 
 def plot_graph(sepal_arr:List[float], petal_arr:List[float], class_arr:List[str]):
     # create plot
@@ -89,12 +120,14 @@ def plot_graph(sepal_arr:List[float], petal_arr:List[float], class_arr:List[str]
     plt.ylabel("Average(cm)")
 
     # set the x-axis with string
-    x = [1, 2, 3]
+    x = []
+    for i in range(len(class_arr)):
+        x.append(i + 1)
     plt.xticks(x, class_arr)
 
     # set range
     plt.ylim(min(sepal_arr) - 0.5, max(sepal_arr) + 0.5)
-    plt.xlim(0.5, 3.5)
+    plt.xlim(0.5, len(class_arr) + 0.5)
 
     plt.plot(x, sepal_arr, x, sepal_arr, 'ro')
     # label the point val
@@ -111,7 +144,7 @@ def plot_graph(sepal_arr:List[float], petal_arr:List[float], class_arr:List[str]
 
     # set range
     plt.ylim(min(petal_arr) - 0.5, max(petal_arr) + 0.5)
-    plt.xlim(0.5, 3.5)
+    plt.xlim(0.5, len(class_arr) + 0.5)
 
     plt.xticks(x, class_arr)
     plt.plot(x, petal_arr, x, petal_arr, 'ro')
@@ -130,6 +163,7 @@ def do_polt(file_name: str):
 
     # sort the data for three class
     format_data = parse_csv_data(data)
+    # format_data = parse_csv_data_adv(data)
 
     # get the average data
     sepal_arr, petal_arr, class_arr = proccess_data(format_data)
@@ -137,6 +171,7 @@ def do_polt(file_name: str):
     # use average data to plot
     plot_graph(sepal_arr, petal_arr, class_arr)
 
+    return
 
 def main():
     # Default file
@@ -146,7 +181,6 @@ def main():
 
 
     do_polt(file_name)
-    print(sys.argv)
 
 
 if __name__ == '__main__':
